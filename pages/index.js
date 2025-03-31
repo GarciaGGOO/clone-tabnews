b19dff2f4b2668691d4e677b0d11015022f12b45
Guilherme Garcia Ortega de Oliveira <111614180+GarciaGGOO@users.noreply.github.com>
2025-03-31 18:24:14 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App</h1>
+      <p>This is a simple example of a Next.js application.</p>
+    </div>
+  );
+}
+
+export default Home;
