3216633aa05958900b53b46c9cc69ea0288a7a0d
Guilherme Garcia Ortega de Oliveira <111614180+GarciaGGOO@users.noreply.github.com>
2025-03-31 20:09:55 +0000

    atualiza mensagem da página inicial com conteúdo pessoal

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 function Home() {
   return (
     <div>
-      <h1>Welcome to My Next.js App</h1>
-      <p>This is a simple example of a Next.js application.</p>
+      <h1>Desculpa Pai</h1>
+      <p>
+        Desculpa Pai, eu fico com medo do futuro e isso me deixa estressado,
+        tudo esta dando erro não esta saída como eu quero, me sinto perdido não
+        sei oq fazer e isso ta me sobrecarregando
+      </p>
+      <p>
+        não sei se vc vai ver isso, mas eu espero que sim, eu só queria que vc
+        soubesse que eu estou tentando fazer o meu melhor, mas as coisas não
+        estão saindo como eu planejei e isso me deixa triste e frustrado
+      </p>
     </div>
   );
 }
