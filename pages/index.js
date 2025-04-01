c9f45038e3a951f3fc8dcbba9e63378437f1e50e
Guilherme Garcia Ortega de Oliveira <111614180+GarciaGGOO@users.noreply.github.com>
2025-04-01 11:06:38 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,9 @@
 function Home() {
   return (
     <div>
-      <h1>Desculpa Pai</h1>
-      <p>
-        Desculpa Pai, eu fico com medo do futuro e isso me deixa estressado,
-        tudo esta dando erro não esta saída como eu quero, me sinto perdido não
-        sei oq fazer e isso ta me sobrecarregando
-      </p>
-      <p>
-        não sei se vc vai ver isso, mas eu espero que sim, eu só queria que vc
-        soubesse que eu estou tentando fazer o meu melhor, mas as coisas não
-        estão saindo como eu planejei e isso me deixa triste e frustrado
-      </p>
+      <h1>Tabnews</h1>
+      <textarea name="" id="" cols="30"></textarea>
+      <button>Enviar</button>
     </div>
   );
 }
