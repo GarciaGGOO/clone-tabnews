ae225b49c771d01605c34bf444f18900758c8565
Guilherme Garcia Ortega de Oliveira <111614180+GarciaGGOO@users.noreply.github.com>
2025-09-17 17:51:23 +0000

    adicionado pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function Home() {
+  const [text, setText] = useState("");
+
+  const handleButton = () => {
+    setText("Acho bom, eu mando e você obedece");
+  }
+
+  return (
+    <div>
+      <h2>Aperte o botão</h2>
+      <button onClick={handleButton}>Clique aqui</button>
+      {text && <h1>{text}</h1>}
+    </div>
+  );
+}
+
+export default Home;
