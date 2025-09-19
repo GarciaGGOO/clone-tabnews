011951e4cbc20e9cabf2ce69dbed78c46b8d0693
Guilherme Garcia Ortega de Oliveira <111614180+GarciaGGOO@users.noreply.github.com>
2025-09-19 13:21:39 +0000

    add arquivos database.js e .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(resquest, response) {
+import database from "../../../../infra/database.js";
+
+async function status(resquest, response) {
+  const result = await database.query("SELECT 1 + 1 as sun;");
+  console.log(result.rows);
   response.status(200).json({ mensagem: "Esse endpoint de status funcionol" });
 }
 
